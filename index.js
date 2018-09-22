a5a17cdff4a55fd190abdda817c5bc6426800a35
Ezra T Huffman <joshooha@gmail.com>
2018-09-22 20:22:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById('div#nested div.target');
+  return document.querySelector('div#nested div.target');
 }
